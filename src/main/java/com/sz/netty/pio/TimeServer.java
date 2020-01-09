cbbdc1b21c1e16a9fdb6b67d2ca84dc64b430f7f
yanghaojia <2453883990@qq.com>
2020-01-09 17:36:29 +0800

    伪异步I/O代码

--- src/main/java/com/sz/netty/pio/TimeServer.java
+++ src/main/java/com/sz/netty/pio/TimeServer.java
@@ -1,4 +1,6 @@
-package com.sz.netty.bio;
+package com.sz.netty.pio;
+
+import com.sz.netty.bio.TimeServerHandler;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -26,9 +28,11 @@ public class TimeServer {
         try (ServerSocket server = new ServerSocket(port)) {
             System.out.println("The time server is start in port : " + port);
             Socket socket = null;
+            // 创建I/O任务线程池
+            TimeServerHandlerExecutePool singleExecutor = new TimeServerHandlerExecutePool(50, 10000);
             while (true) {
                 socket = server.accept();
-                new Thread(new TimeServerHandler(socket)).start();
+                singleExecutor.execute(new TimeServerHandler(socket));
             }
         } finally {
             System.out.println("The time server close");
